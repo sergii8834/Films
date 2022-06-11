0bd3979cef2a346b0806997bfd712400aa0d142b
Serhiy <example@gmail.com>
2022-06-11 13:16:25 +0300

    Phone

--- script.js
+++ script.js
@@ -19,3 +19,36 @@ personalMovieBD.movies[lastWatchedMovies] = scoreOfMovies;
 personalMovieBD.movies[lastWatchedMovies2] = scoreOfMovies2;
 
 console.log(personalMovieBD);
+
+let clientAmount = 5000;
+let amount = 0;
+const iPhonePrice = 2000;
+const iPhoneAmount = 10;
+const iPhoneCase = 400;
+const taxRate = 0.08;
+
+function calculateTax(amount) {
+    return amount * taxRate;
+}
+
+function formatAmount(amount) {
+    return "$" + amount.toFixed(2);
+}
+
+while (amount < clientAmount) {
+    amount = amount + iPhonePrice;
+
+    if (amount < clientAmount) {
+        amount = amount + iPhoneCase;
+    }
+}
+
+amount = amount + calculateTax(amount);
+
+console.log(
+    "Ваша покупка:" + formatAmount(amount)
+);
+
+if (amount > clientAmount) {
+    console.log("Ви не можете дозволити собі цю покупку. :(");
+}
